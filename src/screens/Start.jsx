27992ff2b28df83e86d7167a5496e0204d337c14
mr-phlames <mickdd22@gmail.com>
2019-06-05 04:13:36 +0000

    created /start and removed some html pages

--- src/screens/Start.jsx
+++ src/screens/Start.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
+import './pages.css';
+import Logo from './../assets/img/logo.png';
+
+import One from './../assets/img/anotherbg.jpg';
+import Two from './../assets/img/coolbg.jpg';
+import Three from './../assets/img/loginbg.jpg';
+
 export default function Start(props) {
     return props.hasAuth ? <Redirect to="/home" /> :  (
         <StartComponent />
@@ -8,5 +15,24 @@ export default function Start(props) {
 }
 
 function StartComponent() {
-    return <Link to="/auth/register">Get Started</Link>
+    return (
+        <div>
+            <center>
+                <img src={Logo} className="start-logo" />
+            </center>
+            <div className="start-slider">
+                
+            </div>
+            <center className="start-captions">
+                <p className="h1">This is Portal Online</p>
+                <p>
+
+                </p>
+            </center>
+            <div className="start-buttons">
+                <Link to="/auth/login" className="start-login-btn start-btn btn btn-lg">Log In</Link>
+                <Link to="/auth/register" className="start-register-btn start-btn btn btn-lg">Sign Up</Link>
+            </div>
+        </div>
+    );
 }
